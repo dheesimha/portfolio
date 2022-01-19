68010bcf01a2a733f3caea2da408002633193b8b
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2022-01-20 00:01:23 +0530

    Tab switch

--- src/components/Header/IndividualTab.js
+++ src/components/Header/IndividualTab.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./IndividualTab.css";
 
-function IndividualTab() {
+function IndividualTab(props) {
+  const [isActive, setIsActive] = useState(false);
+  const { name, id } = props;
+
+  const colorChange = () => {
+    setIsActive(!isActive);
+  };
   return (
-    <div className="singleTab">
-      <h2>Profile</h2>
+    <div
+      className="{isActive ? demo : singleTab}"
+      id={id}
+      onClick={colorChange}
+    >
+      <h2>{name}</h2>
     </div>
   );
 }
--- src/components/Header/TabContainer.js
+++ src/components/Header/TabContainer.js
@@ -5,10 +5,11 @@ import "./TabContainer.css";
 function TabContainer() {
   return (
     <div className="tabContainer">
-      <IndividualTab />
-      <IndividualTab />
-      <IndividualTab />
-      <IndividualTab />
+      <IndividualTab name="_home" key="tab-1" id="tab-1" />
+      <IndividualTab name="_about me" key="tab-2" id="tab-2" />
+      <IndividualTab name="_projects" key="tab-3" id="tab-3" />
+      <IndividualTab name="_resume" key="tab-4" id="tab-4" />
+      <IndividualTab name="_contact" key="tab-5" id="tab-5" />
     </div>
   );
 }
