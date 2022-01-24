4d27f9c128a666bd20680e5ced788d8f53a46932
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2022-01-24 11:27:07 +0530

    Complete header finally :)

--- src/components/Header/TabContainer.js
+++ src/components/Header/TabContainer.js
@@ -1,41 +1,88 @@
-import React from "react";
-import IndividualTab from "./IndividualTab";
+import React, { useState } from "react";
 import "./TabContainer.css";
 
 function TabContainer() {
+  const [tab1, setTab1] = useState(true);
+  const [tab2, setTab2] = useState(false);
+  const [tab3, setTab3] = useState(false);
+  const [tab4, setTab4] = useState(false);
+  const [tab5, setTab5] = useState(false);
 
+  const tab1ClickHandler = () => {
+    setTab1(true);
+    setTab2(false);
+    setTab3(false);
+    setTab4(false);
+    setTab5(false);
+  };
+
+  const tab2ClickHandler = () => {
+    setTab1(false);
+    setTab2(true);
+    setTab3(false);
+    setTab4(false);
+    setTab5(false);
+  };
+
+  const tab3ClickHandler = () => {
+    setTab1(false);
+    setTab2(false);
+    setTab3(true);
+    setTab4(false);
+    setTab5(false);
+  };
+
+  const tab4ClickHandler = () => {
+    setTab1(false);
+    setTab2(false);
+    setTab3(false);
+    setTab4(true);
+    setTab5(false);
+  };
+
+  const tab5ClickHandler = () => {
+    setTab1(false);
+    setTab2(false);
+    setTab3(false);
+    setTab4(false);
+    setTab5(true);
+  };
   return (
     <div className="tabContainer">
-      <IndividualTab
-        name="_home"
-        key="tab-1"
-        id="tab-1"
+      <div
+        className={tab1 ? "active singleTab" : "singleTab"}
+        onClick={tab1ClickHandler}
+      >
+        <h2>_home</h2>
+      </div>
 
-      />
-      <IndividualTab
-        name="_about me"
-        key="tab-2"
-        id="tab-2"
+      <div
+        className={tab2 ? "active singleTab" : "singleTab"}
+        onClick={tab2ClickHandler}
+      >
+        <h2>_about me</h2>
+      </div>
 
-      />
-      <IndividualTab
-        name="_projects"
-        key="tab-3"
-        id="tab-3"
+      <div
+        className={tab3 ? "active singleTab" : "singleTab"}
+        onClick={tab3ClickHandler}
+      >
+        <h2>_projects</h2>
+      </div>
 
-      />
-      <IndividualTab
-        name="_resume"
-        key="tab-4"
-        id="tab-4"
+      <div
+        className={tab4 ? "active singleTab" : "singleTab"}
+        onClick={tab4ClickHandler}
+      >
+        <h2>_resume</h2>
+      </div>
 
-      />
-      <IndividualTab
-        name="_contact"
-        key="tab-5"
-        id="tab-5"
-
-      />
+      <div
+        className={tab5 ? "active singleTab" : "singleTab"}
+        onClick={tab5ClickHandler}
+      >
+        <h2>_contact</h2>
+      </div>
     </div>
   );
 }
