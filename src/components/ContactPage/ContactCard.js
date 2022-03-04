b1020887b53f80817f6e4996dd8f2821d4b73efb
Dheemanth Narasimha <dheemanth.simha01@gmail.com>
2022-03-04 13:37:27 +0530

    Complete contact page

--- src/components/ContactPage/ContactCard.js
+++ src/components/ContactPage/ContactCard.js
@@ -3,11 +3,11 @@ import "./ContactCard.css";
 function ContactCard(props) {
   return (
     <div className="contact_card">
-      <p>{props.title}</p>
+      <p class="title">{props.title}</p>
       <a href={props.link} target="_blank" rel="noreferrer">
         <img src={props.source} alt={props.altTag} />
       </a>
-      <p>{props.text}</p>
+      <p class="content-text">{props.text}</p>
     </div>
   );
 }
